f0c80b037270c1f01995391adcc1ee2e3a4be15d
NoritoKutsuzawa <norito.kutsuzawa@gmail.com>
2022-10-15 00:07:49 +0900

    modi axios import

--- src/Row.tsx
+++ src/Row.tsx
@@ -1,4 +1,4 @@
-import axios from "axios";
+import axios from "./axios";
 import { useEffect, useState } from "react";
 
 type Props = {
@@ -23,13 +23,16 @@ export const Row = ({ title, fetchUrl }: Props) => {
     //TODO::functionを書き換える
     async function fetchData() {
       const request = await axios.get(fetchUrl);
+      console.log("URL?", fetchUrl, axios);
+
       setMovies(request.data.results);
       return request;
     }
+
     fetchData();
   }, [fetchUrl]);
 
   console.log(movies);
 
-  return <div className="Row" />;
+  return <div className="Row">{title}</div>;
 };
