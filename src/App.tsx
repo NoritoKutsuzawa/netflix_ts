f50012469e3cda7a545b84cd53592fe824242daa
NoritoKutsuzawa <norito.kutsuzawa@gmail.com>
2022-10-14 23:41:34 +0900

    modi top

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,16 @@
-import React from "react";
-import logo from "./logo.svg";
 import "./App.css";
+import { Row } from "./Row";
+import { requests } from "./request";
 
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      <Row
+        title={"NETFLIX ORIGINALS"}
+        fetchUrl={requests.feachNetflixOriginals}
+      />
+    </div>
+  );
 }
 
 export default App;
