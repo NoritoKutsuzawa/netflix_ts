b5feffc9f06ca0da29f9bee24f1834a86f8eb8b6
NoritoKutsuzawa <norito.kutsuzawa@gmail.com>
2022-10-14 23:42:51 +0900

    add rows to top

--- src/App.tsx
+++ src/App.tsx
@@ -9,6 +9,12 @@ function App() {
         title={"NETFLIX ORIGINALS"}
         fetchUrl={requests.feachNetflixOriginals}
       />
+      <Row title="Top Rated" fetchUrl={requests.feactTopRated} />
+      <Row title="Action Movies" fetchUrl={requests.feactActionMovies} />
+      <Row title="Comedy Movies" fetchUrl={requests.feactComedyMovies} />
+      <Row title="Horror Movies" fetchUrl={requests.feactHorrorMovies} />
+      <Row title="Romance Movies" fetchUrl={requests.feactRomanceMovies} />
+      <Row title="DOcumentaries" fetchUrl={requests.feactDocumentMovies} />
     </div>
   );
 }
